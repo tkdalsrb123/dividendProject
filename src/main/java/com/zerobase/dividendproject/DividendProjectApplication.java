07cf455ca3533b9a73990f68f57967d42ef152d0
woojin <tkdalsrb147@naver.com>
2024-12-08 23:07:18 +0900

    initial build

---
+++ src/main/java/com/zerobase/dividendproject/DividendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.dividendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendProjectApplication.class, args);
+    }
+
+}
